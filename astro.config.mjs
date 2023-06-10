f6e0896af3a860e0861bf4b16ada9ebd2f05d6c6
K3ndev <racelisjk@gmail.com>
2023-06-11 07:22:03 +0800

    setup config for ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
 import { defineConfig } from 'astro/config';
 import solidJs from "@astrojs/solid-js";
-
 import tailwind from "@astrojs/tailwind";
 
+
 // https://astro.build/config
 
 // https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs(), tailwind()]
+  integrations: [solidJs(), tailwind()],
+  output: 'server',
+  adapter: vercel()
 });
