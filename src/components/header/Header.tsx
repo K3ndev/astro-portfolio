4dfae95bfcc793ec0c8fefac4c532ed01fa56cc6
K3ndev <racelisjk@gmail.com>
2023-08-07 18:09:31 +0800

    feat: added top-scroll loading animation

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -10,53 +10,62 @@ export function Header(props: headerProps) {
   const { home, guestBook, gists } = props;
 
   return (
-    <header class="container mx-auto max-w-2xl px-4">
-      <nav class="text-md mb-16 mt-8 flex w-full items-center justify-between">
-        <ul class="flex gap-1">
-          <li>
+    <>
+      <div class="w-100 sticky top-0">
+        <div
+          class="h-1 bg-cyan-700"
+          id="banner-loading"
+          style="width: 0%;"
+        ></div>
+      </div>
+      <header class="container mx-auto max-w-2xl px-4">
+        <nav class="text-md mb-16 mt-8 flex w-full items-center justify-between">
+          <ul class="flex gap-1">
+            <li>
+              <a
+                href="/"
+                class={`cursor-pointer ${
+                  home ? "text-white" : "text-gray-400"
+                } rounded-lg p-1 transition duration-300 ease-out hover:bg-gray-800 md:px-3 md:py-2`}
+              >
+                Home
+              </a>
+            </li>
+            <span class="text-gray-400">/</span>
+            <li>
+              <a
+                href="/gists/"
+                class={`cursor-pointer ${
+                  gists ? "text-white" : "text-gray-400"
+                } rounded-lg p-1 transition duration-300 ease-out hover:bg-gray-800 md:px-3 md:py-2`}
+              >
+                Gist
+              </a>
+            </li>
+            <span class="text-gray-400">/</span>
+            <li>
+              <a
+                href="/guestBook/"
+                class={`cursor-pointer ${
+                  guestBook ? "text-white" : "text-gray-400"
+                } rounded-lg p-1 transition duration-300 ease-out hover:bg-gray-800 md:px-3 md:py-2`}
+              >
+                GuestBook
+              </a>
+            </li>
+          </ul>
+          <div>
             <a
-              href="/"
-              class={`cursor-pointer ${
-                home ? "text-white" : "text-gray-400"
-              } rounded-lg p-1 transition duration-300 ease-out hover:bg-gray-800 md:px-3 md:py-2`}
+              class="block cursor-pointer rounded-lg border border-gray-700 bg-gray-700 p-2 transition duration-300 ease-out hover:border hover:border-gray-400"
+              href="https://k3ndev-performance.netlify.app/k3ndev/"
+              target="_blank"
+              aria-label="Benchmark K3ndev Personal web site projects"
             >
-              Home
+              <BsSpeedometer class="text-xl" />
             </a>
-          </li>
-          <span class="text-gray-400">/</span>
-          <li>
-            <a
-              href="/gists/"
-              class={`cursor-pointer ${
-                gists ? "text-white" : "text-gray-400"
-              } rounded-lg p-1 transition duration-300 ease-out hover:bg-gray-800 md:px-3 md:py-2`}
-            >
-              Gist
-            </a>
-          </li>
-          <span class="text-gray-400">/</span>
-          <li>
-            <a
-              href="/guestBook/"
-              class={`cursor-pointer ${
-                guestBook ? "text-white" : "text-gray-400"
-              } rounded-lg p-1 transition duration-300 ease-out hover:bg-gray-800 md:px-3 md:py-2`}
-            >
-              GuestBook
-            </a>
-          </li>
-        </ul>
-        <div>
-          <a
-            class="block cursor-pointer rounded-lg border border-gray-700 bg-gray-700 p-2 transition duration-300 ease-out hover:border hover:border-gray-400"
-            href="https://k3ndev-performance.netlify.app/k3ndev/"
-            target="_blank"
-            aria-label="Benchmark K3ndev Personal web site projects"
-          >
-            <BsSpeedometer class="text-xl" />
-          </a>
-        </div>
-      </nav>
-    </header>
+          </div>
+        </nav>
+      </header>
+    </>
   );
 }
