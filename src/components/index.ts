3b7d59f219ccc8bbb277bb6ff0cf47a0728aa8ef
K3ndev <racelisjk@gmail.com>
2023-08-31 10:58:54 +0800

    refactor: header tsx into astro component and remove all solid icons

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,4 @@
 export { Comment } from "./comment/index";
 export { ProjectList } from "./projectList/index";
-export { Header } from "./header/Header";
 export { GistList } from "./gistList/index"
 export { WrapperAnimation } from "./wrapperAnimation/index"
--- src/components/projectList/index.tsx
+++ src/components/projectList/index.tsx
@@ -1,8 +1,5 @@
 import { For, onMount, createSignal } from "solid-js";
-import { FaRegularFileCode } from "solid-icons/fa";
-import { AiOutlineArrowRight } from "solid-icons/ai";
 import { Loading } from "./Loading";
-import { BiRegularLinkExternal } from 'solid-icons/bi'
 
 type DataType = {
   name: string;
@@ -69,7 +66,7 @@ export function ProjectList() {
             <div class="flex w-full gap-3">
               <div class="flex items-center">
                 <a href={item.html_url} target="_blank" aria-label={`Github Link to ${item.name}`}>
-                  <FaRegularFileCode class="cursor-pointer text-xl hover:fill-cyan-600" />
+                  <svg class="cursor-pointer text-xl hover:fill-cyan-600" fill="currentColor" stroke-width="0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" height="1em" width="1em" style="overflow: visible;"><path d="M64 464c-8.8 0-16-7.2-16-16V64c0-8.8 7.2-16 16-16h160v80c0 17.7 14.3 32 32 32h80v288c0 8.8-7.2 16-16 16H64zM64 0C28.7 0 0 28.7 0 64v384c0 35.3 28.7 64 64 64h256c35.3 0 64-28.7 64-64V154.5c0-17-6.7-33.3-18.7-45.3l-90.6-90.5C262.7 6.7 246.5 0 229.5 0H64zm97 289c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0L79 303c-9.4 9.4-9.4 24.6 0 33.9l48 48c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-31-31 31-31zm96-34c-9.4-9.4-24.6-9.4-33.9 0s-9.4 24.6 0 33.9l31 31-31 31c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l48-48c9.4-9.4 9.4-24.6 0-33.9l-48-48z"></path></svg>
                 </a>
               </div>
               <div>
@@ -88,11 +85,11 @@ export function ProjectList() {
             <div class="flex items-start pt-2">
               {item.homepage !== "" ? (
                 <a href={item.homepage} target="_blank" aria-label={`Website Link to ${item.name}`}>
-                  <BiRegularLinkExternal class="cursor-pointer text-xl hover:fill-cyan-600" />
+                  <svg class="cursor-pointer text-xl hover:fill-cyan-600" fill="currentColor" stroke-width="0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" height="1em" width="1em" style="overflow: visible;"><path d="m13 3 3.293 3.293-7 7 1.414 1.414 7-7L21 11V3z"></path><path d="M19 19H5V5h7l-2-2H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2h14c1.103 0 2-.897 2-2v-5l-2-2v7z"></path></svg>
                 </a>
               ) : (
                 <p>
-                  <BiRegularLinkExternal class="inline cursor-pointer text-xl hover:cursor-not-allowed" />
+                  <svg class="inline cursor-pointer text-xl hover:cursor-not-allowed" fill="currentColor" stroke-width="0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" height="1em" width="1em" style="overflow: visible;"><path d="m13 3 3.293 3.293-7 7 1.414 1.414 7-7L21 11V3z"></path><path d="M19 19H5V5h7l-2-2H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2h14c1.103 0 2-.897 2-2v-5l-2-2v7z"></path></svg>
                 </p>
               )}
             </div>
@@ -101,12 +98,13 @@ export function ProjectList() {
       </For>
       <div class="flex items-center gap-1">
         <a
-        class="inline text-slate-400 transition duration-300 ease-out hover:text-cyan-600 hover:underline"
-        href="https://github.com/K3ndev?tab=repositories"
-        target="_blank"
-      >
-        View all Projects <AiOutlineArrowRight class="inline"/>
-      </a>
+          class="inline text-slate-400 transition duration-300 ease-out hover:text-cyan-600 hover:underline"
+          href="https://github.com/K3ndev?tab=repositories"
+          target="_blank"
+        >
+          View all Projects
+          <svg class="inline" fill="currentColor" stroke-width="0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024" height="1em" width="1em" style="overflow: visible;"><path d="M869 487.8 491.2 159.9c-2.9-2.5-6.6-3.9-10.5-3.9h-88.5c-7.4 0-10.8 9.2-5.2 14l350.2 304H152c-4.4 0-8 3.6-8 8v60c0 4.4 3.6 8 8 8h585.1L386.9 854c-5.6 4.9-2.2 14 5.2 14h91.5c1.9 0 3.8-.7 5.2-2L869 536.2a32.07 32.07 0 0 0 0-48.4z"></path></svg>
+        </a>
       </div>
     </section>
   );
