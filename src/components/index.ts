99e8ca79f4eb87aa7a19e7580bffa106a0a10dda
K3ndev <racelisjk@gmail.com>
2023-09-08 20:49:56 +0800

    refactor: convert the header to TSX format due to weird behavior during the after-swap from view transition

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { Comment } from "./comment/index";
 export { ProjectList } from "./projectList/index";
 export { GistList } from "./gistList/index"
 export { WrapperAnimation } from "./wrapperAnimation/index"
+export { Header } from "./header/index"
