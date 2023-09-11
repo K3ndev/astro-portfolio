992dfe2ca55316431fc49e9ae5b32013e4186363
K3ndev <racelisjk@gmail.com>
2023-09-11 19:22:07 +0800

    test: add e2e tests for navigation

---
+++ tests/portfolio.spec.ts
@@ -0,0 +1,51 @@
+import { test, expect, type Page } from '@playwright/test';
+
+
+test.describe('navigation', () => {
+    test.beforeEach(async ({ page }) => {
+        // Go to the starting url before each test.
+        await page.goto('https://astro-k3ndev.vercel.app/');
+    });
+  
+    test('main navigation', async ({ page }) => {
+
+        // Expectations for the main navigation test:
+        // 1. Check if the URL matches the starting URL.
+        await expect(page).toHaveURL('https://astro-k3ndev.vercel.app/');
+
+        // 2. Check if the page title is 'Portfollio'.
+        const pageTitle = await page.title();
+        expect(pageTitle).toBe('Portfollio');
+    });
+
+    test('gist navigation', async ({ page }) => {
+
+        // Click Gist link.
+        await page.getByRole('link', { name: 'Gist' }).click();
+
+
+        // 1. Check if the URL matches the expected URL after clicking.
+        await expect(page).toHaveURL('https://astro-k3ndev.vercel.app/gists/');
+
+        // Expectations for the gist navigation test:
+
+        // 2. Check if the page title is 'GuestBook'.
+        const pageTitle = await page.title();
+        expect(pageTitle).toBe('GuestBook');
+    });
+
+    test('guestBook navigation', async ({ page }) => {
+
+        // Click GuestBook link.
+        await page.getByRole('link', { name: 'GuestBook' }).click();
+
+        // Expectations for the guestBook navigation test:
+
+        // 1. Check if the URL matches the expected URL after clicking.
+        await expect(page).toHaveURL('https://astro-k3ndev.vercel.app/guestBook/');
+
+        // 2. Check if the page title is 'GuestBook'.
+        const pageTitle = await page.title();
+        expect(pageTitle).toBe('GuestBook');
+    });
+  });
